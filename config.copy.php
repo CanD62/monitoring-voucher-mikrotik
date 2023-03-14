d8d0edce9fe00405fc9cb33a294ab46859f4f15c
CanD62 <puputz.online@gmail.com>
2023-03-14 09:47:40 +0700

    config copy

---
+++ config.copy.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting(0);
+// check url
+
+ob_start("ob_gzhandler");
+
+$iphost = '******';
+$userhost = '*******';
+$passwdhost = '*******';
+
+
+$hotspotname = 'NaWa.NET';
+$prof = 'unl-6jam-2000';
+
+
+
